930bd7fdf5f28935d092090b84b7f27228189728
burkayelbek <burkay.elbek@gmail.com>
2021-09-19 14:38:29 +0300

    Template and static files and inheritance implemented

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Master-Page yapısı için belirtmemiz gerekiyor.
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,6 +112,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [     # Applerin içerisine bağlı olmayan static dosyalarımız yoksa dışarıdaki klasörde tutuyoruz
+    BASE_DIR / "static"  # Static dosyamızın ana directory içerisinde olduğunu gösterdik.
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
